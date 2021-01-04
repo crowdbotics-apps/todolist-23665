bd1809aa57a345948e2d0489377ff916ec7383ad
Crowdbotics <team@crowdbotics.com>
2021-01-04 04:44:41 +0000

    Update models

--- backend/todolist_23665/wsgi.py
+++ backend/todolist_23665/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todolist_23665.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todolist_23665.settings")
 
 application = get_wsgi_application()
